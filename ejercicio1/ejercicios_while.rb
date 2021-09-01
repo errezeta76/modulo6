14353818292091e221b7bfe78ae96c32273f9e28
errezeta76 <rzc.info@gmail.com>
2021-09-01 09:06:02 -0700

    ejercicios 1-9-2021

--- ejercicio1/ejercicios_while.rb
+++ ejercicio1/ejercicios_while.rb
@@ -9,17 +9,44 @@
 # puts "ingrese sus ventas"
 # venta = gets.chomp.to_i
 
-ventas = 0
-venta = 0
- while venta != -1
-    puts "ingrese sus ventas"
-    venta = gets.chomp.to_i
-    if venta != -1
-     ventas = ventas + venta
-     puts "tus ventas son: #{ventas} " 
-    end  
-end 
-  puts "tus ventas totales son: #{ventas} "
+# ventas = 0
+# venta = 0
+#  while venta != -1
+#     puts "ingrese sus ventas"
+#     venta = gets.chomp.to_i
+#     if venta != -1
+#      ventas = ventas + venta
+#      puts "tus ventas son: #{ventas} " 
+#     end  
+# end 
+#   puts "tus ventas totales son: #{ventas} "
+
+
+# vida = rand(100..500)
+# hp = vida
+# ata_pares = 0
+# ata_inpar = 0
+# if vida <
+# while vida != 0
+
+
+
+
+
+num = rand (1..20)
+adv = 30
+while adv != num
+puts "adivina el numero del 1 al 20"
+adv = gets.chomp.to_i
+if adv > num
+    puts " epale, te pasaste, vajale un cambio"
+elsif adv < num
+    puts "ponele mas color, estas muy bajo"
+end
+end
+puts "adivinaste el numero es #{num}, te pasaste campeon"
+
+
 
 
 
