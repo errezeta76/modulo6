e1a21b8b4017298bda0327b5ef1bc6d8a7fabe9d
errezeta76 <rzc.info@gmail.com>
2021-09-02 20:13:14 -0700

    2-sep-2021 for y array

---
+++ Ejercicio2/ejercicios_array.rb
@@ -0,0 +1,8 @@
+puts "ingrese un numero del largo del arreglo"
+n = gets.chomp.to_i
+for i in 1..n
+    arreglo[rand(1..10)
+end
+for i in 1..n
+ print arreglo[i]
+end
--- ejercicio1/Ejercicios_for.rb
+++ ejercicio1/Ejercicios_for.rb
@@ -10,36 +10,77 @@
 
 # puts ""
 
-puts "ingrese un numero"
-n = gets.chomp.to_i
- if n % 2 == 0
-     n = n / 2
-     for  i in 1..n
-         print " #{v} _"
-         v += 2
-     end
- else
-         v = 1
-         n = n / 2
-         for  i in 1..n
-            print "#{v} $"
-             v += 2
-         end
+# puts "ingrese un numero"
+# n = gets.chomp.to_i
+# tini = Time.now
+# v = 0
+#  if n % 2 == 0
+#      n = n / 2
+#      for  i in 1..n
+#          print " #{v} _"
+#          v += 2
+#      end
+#  else
+#          v = 1
+#          n = n / 2
+#          for  i in 1..n
+#             print "#{v} $"
+#              v += 2
+#          end
          
- end
- puts "#{v}"
+#  end
+#  puts "#{v}"
+#  tfin = Time.now
+#  resultado = tfin - tini
+#  puts "#{resultado}"
 
 
-    puts "ingrese un numero para la tabla 1 al 10"
-    n = gets.chomp.to_i
-    mul =0
+    # puts "ingrese un numero para la tabla 1 al 10"
+    # n = gets.chomp.to_i
+    # mul =0
 
-    for c in 1..30
-        mul = n * c
+    # for c in 1..30
+    #     mul = n * c
 
-    puts " #{n} * #{c} = #{mul}"
+    # puts " #{n} * #{c} = #{mul}"
 
-    # end
+    # # end
+
+# #factorial
+#     puts " ingrese un numero para realizar el factorial "
+#     fac = gets.chomp.to_i
+#     fac = fac
+#     mulf = 1
+#     for c in 1..fac 
+#         print " #{c} * "
+#         mulf = mulf * c
+#     end
+#     mulf = mulf * c
+#     puts "= #{mulf}"
+
+
+#     # numero loteria
+
+# puts " ingrese numeros para la loteria"
+# puts "minimo"
+# min = gets.chomp.to_i
+# puts "Maximo"
+# max = gets.chomp.to_i
+# ncan = 0
+# puts "cantidad de numeros"
+# ncan = gets.chomp.to_i
+# for x in 1..ncan
+#    print "Sorteo : #{x} " 
+#    puts "N: #{rand(min..max)}"
+   
+# end
+
+
+
+
+
+
+
 
 
 
